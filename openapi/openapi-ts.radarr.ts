bf2d2d84c9b4cfb89de26ae53f5fd685dc0511d1
seansusmilch <seantsusmilch@gmail.com>
2025-08-06 21:45:39 -0500

    refactor: no zod validation in generated apis

--- openapi/openapi-ts.radarr.ts
+++ openapi/openapi-ts.radarr.ts
@@ -4,5 +4,5 @@ export default defineConfig({
   // input: 'https://raw.githubusercontent.com/Radarr/Radarr/develop/src/Radarr.Api.V3/openapi.json',
   input: 'openapi/radarr.openapi.json',
   output: 'src/generated/radarr',
-  plugins: ['@hey-api/client-next', 'zod'],
+  plugins: ['@hey-api/client-next'],
 });
--- openapi/openapi-ts.sonarr.ts
+++ openapi/openapi-ts.sonarr.ts
@@ -4,5 +4,5 @@ export default defineConfig({
   // input: 'https://raw.githubusercontent.com/Sonarr/Sonarr/develop/src/Sonarr.Api.V3/openapi.json',
   input: 'openapi/sonarr.openapi.json',
   output: 'src/generated/sonarr',
-  plugins: ['@hey-api/client-next', 'zod'],
+  plugins: ['@hey-api/client-next'],
 });
