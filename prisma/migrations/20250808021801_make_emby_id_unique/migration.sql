5a3bc4f7b86e49e4ac16ef75ca8b21668d5a6e4d
seansusmilch <seantsusmilch@gmail.com>
2025-08-07 21:50:53 -0500

    chore: add cleanup step to migration for unique embyId constraint, removing media items without an embyId before enforcing uniqueness

--- prisma/migrations/20250808021801_make_emby_id_unique/migration.sql
+++ prisma/migrations/20250808021801_make_emby_id_unique/migration.sql
@@ -1,11 +1,14 @@
 /*
-  Warnings:
-
-  - A unique constraint covering the columns `[embyId]` on the table `media_items` will be added. If there are existing duplicate values, this will fail.
-
-*/
+ Warnings:
+ 
+ - A unique constraint covering the columns `[embyId]` on the table `media_items` will be added. If there are existing duplicate values, this will fail.
+ 
+ */
 -- DropIndex
 DROP INDEX "media_items_embyId_idx";
-
+-- Cleanup: remove media items without an embyId prior to enforcing uniqueness
+DELETE FROM "media_items"
+WHERE "embyId" IS NULL
+  OR TRIM("embyId") = '';
 -- CreateIndex
 CREATE UNIQUE INDEX "media_items_embyId_key" ON "media_items"("embyId");
